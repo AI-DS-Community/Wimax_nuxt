90f524735be9ed0f253deb6a177f11ee6afc56f5
Arjun P <72484657+arjunprakash027@users.noreply.github.com>
2022-08-31 14:56:18 +0530

    changes in footer

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,52 +1,74 @@
 <template>
-    <section id="footer" class="flex flex-wrap shadow-lg justify-center lg:justify-between items-center w-full bg-gray-900 px-6 py-3 z-10">
+    <footer class="bg-gray-900">
+  <div class="px-4 py-16 mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+      <div>
+        <span class="block w-32 h-10 bg-gray-900 rounded-lg"></span>
+
+
+        <div class="flex mt-8 text-gray-500 space-x-6">
+
+          <a class="hover:opacity-75" href="https://www.instagram.com/wimaxclub/" target="_blank" rel="noreferrer">
+            <span class="sr-only"> Instagram </span>
+
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" />
+            </svg>
+          </a>
+
+
+          <a class="hover:opacity-75" href="https://github.com/AI-DS-Community" target="_blank" rel="noreferrer">
+            <span class="sr-only"> GitHub </span>
+
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+            </svg>
+          </a>
+
+        </div>
+      </div>
+
+      <div class="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
         <div>
-            <a href="/" class="text-center text-blue-300 font-semibold text-xl hover:text-blue-400">WiMaX</a>
+          <p class="font-medium">
+            About us
+          </p>
+
+          <nav class="flex flex-col mt-4 text-sm text-gray-500 space-y-2">
+            <a class="hover:opacity-75" href="/#about"> About </a>
+            <a class="hover:opacity-75" href=""> Meet the Team </a>
+          </nav>
         </div>
 
-        <div class="text-blue-300 text-xs invisible lg:visible cursor-pointer">
-            <p>© Copyright 2022. All Rights Reserved.</p>
-            </div>
-        <div class="text-blue-300 text-xs invisible lg:visible cursor-pointer">
-            <a onClick="yourlink();" style="cursor: pointer; cursor: hand;"><a Developed by href="https://www.linkedin.com/in/naveen-kumar-s-921990210/">Naveen</a></a>
+        <div>
+          <p class="font-medium">
+            Things we do
+          </p>
+
+          <nav class="flex flex-col mt-4 text-sm text-gray-500 space-y-2">
+            <a class="hover:opacity-75" href="/events"> Events </a>
+            <a class="hover:opacity-75" href="/projects"> Projects </a>
+          </nav>
         </div>
-        <div class="grid grid-cols-3 gap-4 shadow">
-            <a href="https://github.com/AI-DS-Community" target=”_blank”><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
-            </svg></a>
-            <a href="https://github.com/AI-DS-Community" target=”_blank”><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <rect x="4" y="4" width="16" height="16" rx="4"></rect>
-                <circle cx="12" cy="12" r="3"></circle>
-                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"></line>
-            </svg></a>
-            
+
+        <div>
+          <p class="font-medium">
+            Helpful Links
+          </p>
+
+          <nav class="flex flex-col mt-4 text-sm text-gray-500 space-y-2">
+            <a class="hover:opacity-75" href="/contactus"> Contact </a>
+            <a class="hover:opacity-75" href="mailto:wimaxclub@mail.com"> Mail us </a>
+            <a class="hover:opacity-75" href="/joinus"> Join us </a>
+          </nav>
         </div>
-    </section>
+
+      </div>
+    </div>
+
+    <p class="mt-8 text-xs text-gray-500">
+      &copy; 2022 WiMax
+    </p>
+  </div>
+</footer>
 </template>
-
-<script>
-export default {
-  name: 'FooterComp',
-  props: {
-  }
-}
-
-async function yourlink() {
-    e.preventDefault();};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    #footer {
-        height: 10vh
-    }
-    svg {
-        color: rgb(147 197 253);
-    }
-    svg:hover {
-        cursor: pointer;
-        color: rgb(249 115 22);
-    }
-</style>
